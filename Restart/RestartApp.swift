3db013777778f668d19cdf295c62a2c3e4a41ed6
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-04-22 14:54:14 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Asadullah Behlim on 22/04/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
