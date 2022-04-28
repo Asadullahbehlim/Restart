63efdc1767c90daf78c6853e2e023499d223e96e
Asadullah Behlim <asadullahbehlim@gmail.com>
2022-04-28 18:22:12 +0530

    Home Screen Updated

--- Restart/Screens/Home.swift
+++ Restart/Screens/Home.swift
@@ -12,17 +12,51 @@ struct Home: View {
     
     @AppStorage ("OnboardingScreen") var isOnboardingScreenActive : Bool = false
     
-    
     // MARK: - Body
     var body: some View {
         VStack(spacing: 20) {
-            Text("Home")
+            // MARK: - HEADER
+            Spacer()
             
+        
+            ZStack {
+                Circle()
+                    .stroke(.gray.opacity(0.15), lineWidth: 40)
+                    .frame(width: 260, height: 260, alignment: .center)
+                Circle()
+                    .stroke(.gray.opacity(0.15), lineWidth: 80)
+                    .frame(width: 260, height: 260, alignment: .center)
+                Image("character-2")
+                    .resizable()
+                    .scaledToFit()
+                    .padding()
+                Spacer()
+            }
+            
+            
+            // MARK: - CENTER
+            
+            Text("The time that leads to mastery is dependent on the intensity of our focus")
+                .fontWeight(.light)
+                .font(.title3)
+                .multilineTextAlignment(.center)
+                .foregroundColor(.secondary)
+                .padding()
+              
+            // MARK: - FOOTER
+            Spacer()
             Button(action: {
             isOnboardingScreenActive = true
             }) {
+                Image(systemName: "arrow.triangle.2.circlepath.circle.fill")
+                    .imageScale(.large)
                 Text ("Restart")
+                    .font(.system(.title3, design: .rounded))
+                    .fontWeight(.bold)
             }
+            .buttonStyle(.borderedProminent)
+            .buttonBorderShape(.capsule)
+            .controlSize(.large)
         }
 
     }
@@ -30,5 +64,6 @@ struct Home: View {
 struct Home_Previews: PreviewProvider {
     static var previews: some View {
         Home()
+       //     .preferredColorScheme(.dark)
     }
 }
